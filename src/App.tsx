5559c22b1d6d097f0e278c9b91e5a767bc285fa5
David Cunningham <david7789@gmail.com>
2018-10-12 00:24:05 -0400

    Fixed the background div positioning.

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,11 @@ import { defaultTheme as theme } from './style/themes';
 import DefaultPage from './pages/DefaultPage';
 
 const backgroundStyle = { 
-  backgroundAttachment: "fixed",
-  backgroundImage: theme.backgroundColor, 
-  backgroundRepeat: "no-repeat",
-  display: "fixed",
-  left: 0,
-  right: 0,
-  bottom: 0,
-  top: 0,
-  //height: "100%",
-  zIndex: -99
+  backgroundImage: theme.backgroundColor
 };
 
 class App extends React.Component {
   public render() {
-    
     return (
       <div>
         <div className="app__background" style={backgroundStyle}/>
