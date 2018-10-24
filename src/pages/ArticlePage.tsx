6b6deac9ac5725d3a0249a207cf8037d0ee0c3fd
David Cunningham <david7789@gmail.com>
2018-10-23 23:52:47 -0400

    Added react-router and types.  Added routes for DefaultPage & Article.  Added full article mock data and Article Component logic.

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
 
 import DefaultNavbar from '../parts/DefaultNavbar/DefaultNavbar';
+import Article, { IArticleProps } from '../parts/Article/Article';
+
+import ApiCaller from '../parts/ApiCaller/ApiCaller';
+import { IGetArticleDataResponse, IArticleData, IGetUserDataResponse, IUserData } from '../parts/ApiCaller/ApiCaller.d';
+
+import { RouteComponentProps } from 'react-router';
 
 import { defaultTheme as theme } from '../style/themes';
+//import './ArticlePage.css';
 
-import './ArticlePage.css';
-
-
-interface IArticlePageState {
-    
+interface IArticlePageMatchParams {
+    articleId: string;
 };
 
-interface IArticlePageProps {
-    siteName: string;
-};
+interface IArticlePageProps extends RouteComponentProps<IArticlePageMatchParams>{};
+
+interface IArticlePageState extends IArticleProps {};
+
+// { title, authorName, authorUrl, date, weekday, body }
 
 
 const MARGIN_TOPBOTTOM: number = 20;
@@ -25,15 +31,42 @@ const articlePageStyle = {
 
 
 export default class ArticlePage extends React.Component<IArticlePageProps, IArticlePageState> {
+    constructor(props: IArticlePageProps) {
+        super(props);
+
+        this.state = {} as IArticlePageState;
+    }
+
+    public componentDidMount() {
+        const { articleId } = this.props.match.params;
+        ApiCaller
+            .getArticleData(articleId)
+            .then((articleDataResponse: IGetArticleDataResponse) => {
+                const articleData: IArticleData = articleDataResponse.data;
+                ApiCaller
+                    .getUserData(articleData.authorId)
+                    .then((authorDataResponse: IGetUserDataResponse) => {
+                        const authorData: IUserData = authorDataResponse.data;
+
+                        // TODO: setState for all info
+                        this.setState({
+                            title: articleData.articleName,
+                            authorId: authorData.id,
+                            authorName: authorData.name,
+                            authorUrl: authorData.url,
+                            date: articleData.articleDate,
+                            body: articleData.articleBody
+                        });
+                    });
+            });
+    }
 
     public render() {
         return (
             <div className="article-page" style={articlePageStyle}>
-                <DefaultNavbar siteName={this.props.siteName} />
+                <DefaultNavbar />
 
-                <div>
-                    {/*article data/body*/}
-                </div>
+                <Article {...this.state} />
             </div>
         );
     }
