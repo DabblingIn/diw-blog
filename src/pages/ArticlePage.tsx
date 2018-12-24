b5cd6a3cb09adaff21ff428077b360aff22a60ce
David Cunningham <david7789@gmail.com>
2018-12-23 21:11:20 -0500

    Debug Article Page (#17)
    * Debugged Article and ArticlePage issues post system tests.
    
    * Cleaned up Article CSS.

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -5,7 +5,7 @@ import DefaultNavbar from '../parts/DefaultNavbar/DefaultNavbar';
 import Article, { IArticleProps } from '../parts/Article/Article';
 
 import * as ApiCaller from '../parts/ApiCaller/ApiCaller';
-import { IGetArticleDataResponse, IGetArticleData, IGetUserDataResponse, IGetUserDataReturn } from '../parts/ApiCaller/ApiCaller.d';
+import { IGetArticleDataResponse, IGetArticleData } from '../parts/ApiCaller/ApiCaller.d';
 
 import { getSubdomainConfig } from '../subdomains';
 
@@ -41,29 +41,15 @@ export default class ArticlePage extends React.Component<IArticlePageProps, IArt
         ApiCaller
             .getArticleDataByUrlId(articleUrlId)
             .then((articleDataResponse: IGetArticleDataResponse) => {
-                const articleData: IGetArticleData = articleDataResponse.data.data;
-                // TODO: const articleDataErr: string = res.data.err;  // Use this backend err message if not null
-                // TODO: Remove this extra user call and use the user data from the article call
-                ApiCaller
-                    .getUserData(articleData.authorId)
-                    .then((authorDataResponse: IGetUserDataResponse) => {
-                        const authorData: IGetUserDataReturn = authorDataResponse.data;
+                const data: IGetArticleData = articleDataResponse.data.data;
 
-                        // TODO: setState for all info
-                        this.setState({
-                            title: articleData.articleTitle,
-                            authorId: authorData.userId,
-                            authorName: authorData.userDisplayName,
-                            authorUrl: authorData.userWebsite,
-                            date: articleData.articleCreatedAt,
-                            body: articleData.articleContent
-                        });
-                    });
+                this.setState({ ...data });
+                // TODO: const articleDataErr: string = res.data.err;  // Use this backend err message if not null
             });
     }
 
     public render() {
-        document.title = subdomainConfig.tabName + " | " + this.state.title;
+        document.title = subdomainConfig.tabName + " | " + this.state.articleTitle;
 
         return (
             <div className="article-page" style={articlePageStyle}>
--- src/parts/Article/Article.tsx
+++ src/parts/Article/Article.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
 
 import * as util from '../../util';
+import { IGetArticleData } from '../ApiCaller/ApiCaller.d';
 
 import { defaultTheme as theme } from '../../style/themes';
 import './Article.css';
 
-export interface IArticleProps {
-    title: string;
-    authorId: string;
-    authorName: string;
-    authorUrl: string;
-    date: Date;
-    weekday: string;
-    body: string;
-};
+export interface IArticleProps extends IGetArticleData {};
 
 const articleStyle = theme.itemBoxStyle;
 const titleStyle = theme.articleTitleStyle;
@@ -21,28 +14,20 @@ const titleStyle = theme.articleTitleStyle;
 export default class Article extends React.Component<IArticleProps> {
 
     public render() {
-        const { title, authorId, authorName, authorUrl, date, body } = this.props;
-        let dateString: string;
-        let weekday: string;
-        try {
-            dateString = util.minDateString(date);
-            weekday = util.getWeekdayString(date);
-        } catch(err) {
-            // if dates are not yet populated, return empty frame
-            return <article className="article" style={articleStyle} />;
-        }
+        const articleCreatedAt = new Date(this.props.articleCreatedAt);
+        //const articleUpdatedAt = new Date(this.props.articleUpdatedAt);
         return (
             <article className="article" style={articleStyle}>
-                <h2 className="article__title" style={titleStyle}>{title}</h2>
+                <h2 className="article__title" style={titleStyle}>{this.props.articleTitle}</h2>
                 <div className="article__name-date-line">
                     <span className="article__author-date-box">
-                        <a className="article__author" href={authorUrl}>{authorName} (<b>{authorId}</b>)</a>
-                        <span className="article__date">{dateString}</span>
-                        <span className="article__weekday">{weekday}</span>
+                        <a className="article__author" href={this.props.authorSite}>{this.props.authorName} (<b>{this.props.authorUsername}</b>)</a>
+                        <span className="article__created_date">{util.minDateString(articleCreatedAt)}</span>
+                        <span className="article__weekday">{util.getWeekdayString(articleCreatedAt)}</span>
                     </span>
                 </div>
-                <div className="article__body" 
-                    dangerouslySetInnerHTML={{ __html: body }} />
+                <div className="article__content" 
+                    dangerouslySetInnerHTML={{ __html: this.props.articleContent }} />
             </article>
         );
     }
