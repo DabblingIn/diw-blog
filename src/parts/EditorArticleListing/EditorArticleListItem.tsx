b5ffe0b483bc9d05cb3639934812db4b4dd5ac9e
David Cunningham <david7789@gmail.com>
2019-02-23 14:10:46 -0500

    Added sub reference to EditorArticleListItem's.

--- src/parts/EditorArticleListing/EditorArticleListItem.tsx
+++ src/parts/EditorArticleListing/EditorArticleListItem.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import * as util from '../../util';
 import { IGetArticleListData } from '../ApiCaller/ApiCaller.d';
+import { isMegaSub } from '../../subdomains';
 
 import { defaultTheme as theme } from '../../style/themes';
 import './EditorArticleListItem.css';
@@ -11,12 +12,17 @@ export interface IEditorArticleListItemProps extends IGetArticleListData {}
 
 export default class EditorArticleListItem extends React.Component<IEditorArticleListItemProps, {}> {
     public render() {
+        const aSub = this.props.articleSub;
+        const subHref = isMegaSub(aSub) ? 'http://dabblingin.com' : 'http://' + aSub + '.dabblingin.com';
         return (
             <div className="editor-article-list-item" style={theme.itemBoxStyle}>
                 <a className="editor-article-list-item__link" href={editorArticleLink(this.props.articleId)}>
                     <h3 className="editor-article-list-item__title" style={theme.articleTitleStyle}>{this.props.articleTitle}</h3>
                 </a>
                 <a className="editor-article-list-item__author" href={util.userLink(this.props.authorId)}>{this.props.authorName}</a>
+                <a className="editor-article-list-item__sub" href={subHref}>
+                    {aSub}
+                </a>
                 <p className="editor-article-list-item__description">{this.props.articleDescription}</p>
                 <p className="editor-article-list-item__date">{util.minDateString(this.props.articleUpdatedAt)}</p>
             </div>
