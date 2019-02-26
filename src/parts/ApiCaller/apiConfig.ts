969fc0399145746abe43d8ce7307db98e401cd0a
David Cunningham <david7789@gmail.com>
2019-02-25 23:09:32 -0500

    Updated site-api path in apiConfig (#30)

--- src/parts/ApiCaller/apiConfig.ts
+++ src/parts/ApiCaller/apiConfig.ts
@@ -2,7 +2,7 @@
 const apiConfig = {
     "base": {
         "dev": "http://localhost:9090",
-        "prod": "http://site-api.dblin.co"
+        "prod": "http://site-api.dabblingin.com"
     },
     "MOCK": false    // for now, can change this to turn on/off mock.  Just reset any changes to this file (git checkout apiConfig.ts).
 }
