dd4ac2a37e3ffc52104070c4bd3935b63ed87ce5
David Cunningham <david7789@gmail.com>
2019-06-19 19:30:00 -0400

    Show article sub when displayed on base or different sub. (#52)
    * Switched ArticleListItem to hooks model.
    
    * Added subdomain display for article list items when displayed on the base/different domain.

--- src/parts/ArticleListing/ArticleListItem.tsx
+++ src/parts/ArticleListing/ArticleListItem.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import * as util from '../../util';
+import { getSubKey } from '../../subdomains';
 import { IGetArticleListData } from '../ApiCaller/ApiCaller.d';
 
 import { defaultTheme as theme } from '../../style/themes';
@@ -8,19 +9,37 @@ import './ArticleListItem.css';
 
 const titleStyle = theme.articleTitleStyle;
 
+const currentSub = getSubKey();
+
 export interface IArticleListItemProps extends IGetArticleListData {}
 
-export default class ArticleListItem extends React.Component<IArticleListItemProps, {}> {
-    public render() {
+export default function ArticleListItem(props: IArticleListItemProps) {
+    return (
+        <div className="article-list-item item-box">
+            <a className="article-list-item__link" href={util.articleLink(props.articleUrlId)}>
+                <h3 className="article-list-item__title" style={titleStyle}>{props.articleTitle}</h3>
+            </a>
+            <a className="article-list-item__author" href={util.userLink(props.authorId)}>{props.authorName}</a>
+            <ArticleSub articleSub={props.articleSub} />
+            <p className="article-list-item__description">{props.articleDescription}</p>
+            <p className="article-list-item__date"><span style={{color:'#555'}}>Updated </span>{util.minDateString(props.articleUpdatedAt)}</p>
+        </div>
+    );
+}
+
+interface IArticleSubProps {
+    articleSub: string;
+}
+
+function ArticleSub(props: IArticleSubProps) {
+    if (currentSub === props.articleSub) {
+        return null;
+    } else {
+        const subHref = 'http://' + props.articleSub + '.dabblingin.com';
         return (
-            <div className="article-list-item item-box">
-                <a className="article-list-item__link" href={util.articleLink(this.props.articleUrlId)}>
-                    <h3 className="article-list-item__title" style={titleStyle}>{this.props.articleTitle}</h3>
-                </a>
-                <a className="article-list-item__author" href={util.userLink(this.props.authorId)}>{this.props.authorName}</a>
-                <p className="article-list-item__description">{this.props.articleDescription}</p>
-                <p className="article-list-item__date"><span style={{color:'#555'}}>Updated </span>{util.minDateString(this.props.articleUpdatedAt)}</p>
-            </div>
-        );
+            <a className="article-list-item__sub" href={subHref}>
+                {props.articleSub}
+            </a>
+        )
     }
 }
