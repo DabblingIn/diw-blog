97f16f94fcdeb32e21725a9ba72df77f57d2e696
David Cunningham <david7789@gmail.com>
2019-06-19 19:28:41 -0400

    Added subdomain display for article list items when displayed on the base/different domain.

--- src/parts/ArticleListing/ArticleListItem.tsx
+++ src/parts/ArticleListing/ArticleListItem.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 
 import * as util from '../../util';
+import { getSubKey } from '../../subdomains';
 import { IGetArticleListData } from '../ApiCaller/ApiCaller.d';
 
 import { defaultTheme as theme } from '../../style/themes';
 import './ArticleListItem.css';
 
 const titleStyle = theme.articleTitleStyle;
+
+const currentSub = getSubKey();
 
 export interface IArticleListItemProps extends IGetArticleListData {}
 
@@ -17,8 +20,26 @@ export default function ArticleListItem(props: IArticleListItemProps) {
                 <h3 className="article-list-item__title" style={titleStyle}>{props.articleTitle}</h3>
             </a>
             <a className="article-list-item__author" href={util.userLink(props.authorId)}>{props.authorName}</a>
+            <ArticleSub articleSub={props.articleSub} />
             <p className="article-list-item__description">{props.articleDescription}</p>
             <p className="article-list-item__date"><span style={{color:'#555'}}>Updated </span>{util.minDateString(props.articleUpdatedAt)}</p>
         </div>
     );
+}
+
+interface IArticleSubProps {
+    articleSub: string;
+}
+
+function ArticleSub(props: IArticleSubProps) {
+    if (currentSub === props.articleSub) {
+        return null;
+    } else {
+        const subHref = 'http://' + props.articleSub + '.dabblingin.com';
+        return (
+            <a className="article-list-item__sub" href={subHref}>
+                {props.articleSub}
+            </a>
+        )
+    }
 }
