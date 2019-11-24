6d9f1ab456ada3918ce41511e95935ebca56638c
David Cunningham <david7789@gmail.com>
2019-11-24 14:16:15 -0500

    Changed all site references from dabblingin.com -> dabbling.in

--- src/parts/ApiCaller/apiConfig.ts
+++ src/parts/ApiCaller/apiConfig.ts
@@ -2,7 +2,7 @@
 const apiConfig = {
     "base": {
         "dev": "http://localhost:9090",
-        "prod": "https://site-api.dabblingin.com"
+        "prod": "https://site-api.dabbling.in"
     },
     "MOCK": false    // for now, can change this to turn on/off mock.  Just reset any changes to this file (git checkout apiConfig.ts).
 }
